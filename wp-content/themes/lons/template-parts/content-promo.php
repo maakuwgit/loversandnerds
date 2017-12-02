b49b32bc8618fc0f942c4657f1f89462e43ca1cb
Mark R Williamson <maakuwemail@gmail.com>
2017-12-02 13:25:41 -0800

    Fixed some inconsistencies and added CTAs to the footer

--- wp-content/themes/lons/template-parts/content-promo.php
+++ wp-content/themes/lons/template-parts/content-promo.php
@@ -11,18 +11,24 @@
 		<img alt="" src="<?php echo $dir; ?>hero-users_mobile.jpg" data-src-medium="<?php echo $dir;?>hero-users_tablet.jpg" data-src-large="<?php echo $dir;?>hero-users.jpg" data-src-xlarge="<?php echo $dir;?>hero-users.jpg">
 	</div>
 	<header class="table text-center">
-			<h2>Who are we?</h2>
+		<h2>Who are we?</h2>
 	</header>
-	<div class="table">
+	<div class="table row">
 	<?php 
+		$u = 0;
 		foreach( $users as $user ) :
 			$desc = get_user_meta( $user->ID, 'description' );
+			if( $u%2 == 0 ){
+				$style = ' text-left';
+			}else{
+				$style = ' text-right';
+			}
 	?>
-	<div class="cell small-12 medium-4 large-2 text-center">
-		<p><?php echo $desc[0]; ?></p>
-		<a href="<?php echo $user->user_url;?>" title="See what Mark has been doing" class="button">Learn More</a>
-	</div>
-	<?php endforeach; ?>
+		<div class="column small-12 medium-4 large-2<?php echo $style; ?>">
+			<p><?php echo $desc[0]; ?></p>
+			<p><a href="<?php echo $user->user_url;?>" title="See what Mark has been doing" class="button">Learn More</a></p>
+		</div>
+	<?php $u++; endforeach; ?>
 	</div>
 </article>
 <?php endif; ?>
